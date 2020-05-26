d30b7c519bf17ca99a9e64d449a05f99ff671b1b
github username <github email address>
2020-05-26 09:56:43 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { ActionCableProvider } from 'react-action-cable-provider';
+import { API_WS_ROOT } from './constraints';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ActionCableProvider url={API_WS_ROOT} >
+      <App />
+    </ActionCableProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

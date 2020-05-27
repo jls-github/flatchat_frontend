421120a2da527f4f02854d8a3389be5b3aebfe34
github username <github email address>
2020-05-27 14:13:48 -0700

    Created fetch request to retrieve jwt token

---
+++ src/containers/Home.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import ConversationsContainer from '../containers/conversationsContainer'
+import MessageContainer from '../containers/MessageContainer';
+import Cable from '../components/cable';
+import { ActionCableConsumer } from 'react-actioncable-provider';
+import { API_ROOT, HEADERS } from '../constraints/index'
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+          conversations: [],
+          activeConversation: null
+        }
+      }
+    
+      componentDidMount = () => {
+        fetch(`${API_ROOT}/conversations`)
+        .then(res => res.json())
+        .then(json => this.setState({conversations: json}))
+      }
+    
+      handleClick = activeConversation => {
+        this.setState({activeConversation: activeConversation})
+      }
+    
+      handleReceivedConversation = res => {
+        const { conversation } = res
+        this.setState({conversations: [...this.state.conversations, conversation]})
+      }
+    
+      handleReceivedMessage = res => {
+        console.log("Here I am", res)
+        const {message} = res
+        this.setState(prevState => {
+          const conversations = [...prevState.conversations]
+          const convo = conversations.find(convo => convo.id === message.conversation_id)
+          convo.messages = [...convo.messages, message]
+          this.setState({conversations})
+        })
+      }
+    
+      onAddMessage = (message) => {
+        fetch("http://localhost:3000/messages", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            message: {
+              text: message,
+              conversation_id: this.state.activeConversation.id,
+              user_id: 13
+             } //hard coded - change with auth
+          })
+        })
+      }
+      render() {
+          const {conversations, activeConversation} = this.state
+        return(
+            <fragment>
+    
+                <ActionCableConsumer 
+                    channel={{channel: 'ConversationsChannel'}} 
+                    onReceived={this.handleReceivedConversation} 
+                  />
+                  {this.state.conversations.length ? (
+                    <Cable conversations={conversations} handleReceivedMessage={this.handleReceivedMessage} />
+                  ): null}
+    
+                <ConversationsContainer conversations={conversations} handleClick={this.handleClick}/>
+                {activeConversation ?
+                    <MessageContainer activeConversation={activeConversation} onAddMessage={this.onAddMessage} />
+                : null}
+            </fragment>
+        )
+      }
+    
+}
+
+export default Home

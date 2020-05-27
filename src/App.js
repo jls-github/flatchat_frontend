6e8a6644e497d6303c20114ca9b6963860c6ad21
Roger Robertson Jr <rjrjr@pm.me>
2020-05-27 15:28:00 -0700

    fixed App.js merge conflict

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import ConversationsContainer from './containers/ConversationsContainer'
-import { ActionCableConsumer } from 'react-actioncable-provider';
-import MessageContainer from './containers/MessageContainer';
-import Cable from './components/Cable';
-import { API_ROOT, HEADERS } from './constraints/index'
-
-const actioncable = require("actioncable")
-=======
 import Home from './containers/Home'
 import Login from './components/Login'
 import {BrowserRouter as Router, Route } from 'react-router-dom';
->>>>>>> 8a0f27862fc4038a93616fd7ba8c662a83943d7f
 
 
 

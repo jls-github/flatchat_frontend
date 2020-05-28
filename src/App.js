f4f1db9ef68f0dc12c757031db14c1bbf59361a7
github username <github email address>
2020-05-28 10:07:41 -0700

    User creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './containers/Home'
 import Login from './components/Login'
+import Signup from './components/Signup'
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 // import AuthWrapper from './HOCs/AuthWrapper'
 
@@ -10,9 +11,9 @@ const App = () =>  {
     return (
           <Router>
             <div>
-
               <Route exact path='/' component={Login} />
               <Route path='/home' component={Home}/>
+              <Route path='/signup' component={Signup} />
             </div>
           </Router>
       );
